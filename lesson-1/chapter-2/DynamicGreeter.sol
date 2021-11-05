e1880b7ea9473489fbd82c0f363e6dd1bb4bc18c
aboutindra <me@indra.codes>
2021-11-05 22:55:00 +0700

    [Lesson-1] > [Chapter-2] > Add Dynamic Contract

---
+++ lesson-1/chapter-2/DynamicGreeter.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.0;
+
+contract DynamicGreeter {
+    string private _greeting = "Hello World!";
+
+    function DynamicGreeter() external view returns(string memory){
+        return _greeting;
+    }
+
+    function setDynamicGreeter(string calldata greeting) external {
+        _greeting = greeting;
+    }
+}
