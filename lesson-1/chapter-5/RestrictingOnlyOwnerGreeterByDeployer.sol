827e0ecaf1868cd928576cd40522936b5786e5f6
aboutindra <me@indra.codes>
2021-11-07 03:16:40 +0700

    [Lesson-1] > [Chapter-5] > Add RestrictingOnlyOwnerGreeterByDeployer contract

---
+++ lesson-1/chapter-5/RestrictingOnlyOwnerGreeterByDeployer.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.0;
+
+contract RestrictingOnlyOwnerGreeterByDeployer {
+    string private _greeting = "Hello World!";
+    address private _owner;
+
+    constructor() public {
+        _owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(
+            msg.sender == _owner,
+            "Ownable: caller is not the owner"
+        );
+        _;
+    }
+
+    function greet() external view returns(string memory){
+        return _greeting;
+    }
+
+    function setGreeting(string calldata greeting) external onlyOwner{
+        _greeting = _greeting;
+    }
+
+    function owner() public view returns(address){
+        return _owner;
+    }
+}
